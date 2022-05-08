ac3c81f2cea58f62e4451a15bea5c1d0ee76dd95
lybovi.s <evgenii_milar@list.ru>
2022-05-08 22:39:10 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { data } from './data.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bend-md-project';
+
+   myData = data
+   zones = []
+
+  constructor() {}
+
+  
+  ngOnInit() {
+
+    let sortedDatabyAreaId =  this.groupNew(this.myData) 
+    let groupValues = []  
+
+    groupValues.push(...Object.values(sortedDatabyAreaId))
+
+    for(let card of groupValues){
+      this.zones.push(this.childrenGroupChecker(card))
+    }
+
+  }
+
+  public groupNew (data) {
+ 
+    return data.reduce(function(group, item, index, originData){
+      const { id, areaId } = item;
+
+      if(item.joinedWith == null) {
+        let childs = originData.filter((child) => id == child.joinedWith);
+        if(childs.length) {
+          item['childs'] = childs;
+        }
+        group[areaId] = group[areaId] ?? [];
+        group[areaId].push(item);
+      }
+      return group
+  }, {})
+  }
+
+     // recursive function for create 1 array with card children
+     public childrenGroupChecker(theList) {
+      let newList = [];  
+  
+      theList.map((listItem) => {
+        newList.push(listItem);
+      
+        if (listItem.childs?.length > 0 ) {
+          newList.push(...this.childrenGroupChecker(listItem?.childs));
+        }
+      });
+  
+      return newList;
+    }
+
+ 
 }
